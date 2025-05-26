7f7f908fa20f5284bb967f6cfe401c65fee4e828
jinyizhi <jinyizhi666@jinyizhideMacBook-Air.local>
2025-05-26 21:34:37 +0800

    update config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
@@ -13,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  outDir: 'dist'
   base: '/my-website/', // ⚠️ 改成你的 repo 名
   output: 'static',     // ⚠️ GitHub Pages 只能用 static
   site: 'https://ariastrocyte.github.io/my-website', // ⚠️ 改成你自己的 GitHub Pages 網址
