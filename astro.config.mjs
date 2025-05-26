69eb50b1150a437ed5f2f56308cfea81efc11b8c
jinyizhi <jinyizhi666@jinyizhideMacBook-Air.local>
2025-05-26 22:41:35 +0800

    fix: remove base for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,23 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import markdoc from '@astrojs/markdoc';
+import svelte from '@astrojs/svelte';
 import remarkCodeTitles from 'remark-code-titles';
 
+// 这三行必须写在最前面
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  outDir: 'dist'
-  base: '/my-website/', // ⚠️ 改成你的 repo 名
-  output: 'static',     // ⚠️ GitHub Pages 只能用 static
-  site: 'https://ariastrocyte.github.io/my-website', // ⚠️ 改成你自己的 GitHub Pages 網址
+  outDir: 'dist',
+  //base: '/my-website/',
+  output: 'static',
+  site: 'https://ariastrocyte.github.io/my-website',
 
   markdown: {
     syntaxHighlight: 'shiki',
